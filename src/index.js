a7a58833b18d8839bfe24ab42addaf611f41118d
Dinar Farhutdinov <denarey77@gmail.com>
2024-10-25 13:37:18 +0300

    feat: add solution for the task

--- src/index.js
+++ src/index.js
@@ -38,8 +38,40 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    const arr = []
+    for (let i = 0; i < expr.length; i += 10) {
+      arr.push(expr.substring(i, i + 10));
+    }
+
+    const arrWithoutLeadingZeroes = [];
+    for (let i = 0; i < arr.length; i += 1) {
+      arrWithoutLeadingZeroes.push(arr[i].slice(arr[i].indexOf('1')));
+    }
+
+    const arrWithDotsAndDashes = [];
+    let tempStr = '';
+    for (let i = 0; i < arrWithoutLeadingZeroes.length; i += 1) {
+      tempStr = '';
+      for (let j = 0; j < arrWithoutLeadingZeroes[i].length; j += 2) {
+        if((arrWithoutLeadingZeroes[i][j] === '1') && (arrWithoutLeadingZeroes[i][j + 1] === '0')) tempStr = `${tempStr}.`;
+        if((arrWithoutLeadingZeroes[i][j] === '1') && (arrWithoutLeadingZeroes[i][j + 1] === '1')) tempStr = `${tempStr}-`
+      }
+      arrWithDotsAndDashes.push(tempStr);
+    }
+
+    const arrWithLetters = [];
+    for (let i = 0; i < arrWithDotsAndDashes.length; i += 1) {
+      if (MORSE_TABLE[arrWithDotsAndDashes[i]] === undefined) arrWithLetters.push(' ');
+      else arrWithLetters.push(MORSE_TABLE[arrWithDotsAndDashes[i]]);
+    }
+
+    let strDecoded = '';
+    for (let i = 0; i < arrWithLetters.length; i += 1) {
+      strDecoded = `${strDecoded}${arrWithLetters[i]}`;
+    }
+
+    return strDecoded;
+  }
 
 module.exports = {
     decode
